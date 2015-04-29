886854107e1ea9e6bb04b01c3d9fe957f9ab2769
Jeff Lee <User@Users-MacBook-Pro.local>
2015-04-29 15:19:48 -0400

    button and user location set

--- HalalMapper/ViewController.m
+++ HalalMapper/ViewController.m
@@ -25,18 +25,18 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:-33.86
-                                                            longitude:151.20
-                                                                 zoom:6];
+    GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:40.7286689
+                                                            longitude:-73.99566199999998
+                                                                 zoom:7];
     mapView_ = [GMSMapView mapWithFrame:CGRectZero camera:camera];
     mapView_.myLocationEnabled = YES;
     self.view = mapView_;
     
     // Creates a marker in the center of the map.
     GMSMarker *marker = [[GMSMarker alloc] init];
-    marker.position = CLLocationCoordinate2DMake(-33.86, 151.20);
-    marker.title = @"Sydney";
-    marker.snippet = @"Australia";
+    marker.position = CLLocationCoordinate2DMake(40.7286689, -73.99566199999998);
+    marker.title = @"You";
+    marker.snippet = @"Hungry for Halal";
     marker.map = mapView_;
     /*
     [manager requestAlwaysAuthorization];
