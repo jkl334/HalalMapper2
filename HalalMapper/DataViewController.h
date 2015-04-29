3b5f31c2a216bfa896da56194d8a69283e07bc47
Liam Mellor <liam.r.mellor@gmail.com>
2015-04-29 16:37:56 -0400

    created dataViewController for cart information

---
+++ HalalMapper/DataViewController.h
@@ -0,0 +1,21 @@
+//
+//  DataViewController.h
+//  HalalMapper
+//
+//  Created by Liam Mellor on 4/29/15.
+//  Copyright (c) 2015 nyu.edu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DataViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UILabel *cartAddress;
+@property (weak, nonatomic) IBOutlet UILabel *thumbsUp;
+@property (weak, nonatomic) IBOutlet UILabel *thumbsDown;
+@property (weak, nonatomic) IBOutlet UILabel *drinkBool;
+@property (weak, nonatomic) IBOutlet UILabel *pitaBool;
+@property (weak, nonatomic) IBOutlet UILabel *greenSauceBool;
+
+
+@end
---
+++ HalalMapper/DataViewController.m
@@ -0,0 +1,20 @@
+//
+//  DataViewController.m
+//  HalalMapper
+//
+//  Created by Liam Mellor on 4/29/15.
+//  Copyright (c) 2015 nyu.edu. All rights reserved.
+//
+
+#import "DataViewController.h"
+
+@implementation DataViewController
+
+@synthesize cartAddress;
+@synthesize thumbsUp;
+@synthesize thumbsDown;
+@synthesize drinkBool;
+@synthesize pitaBool;
+@synthesize greenSauceBool;
+
+@end
