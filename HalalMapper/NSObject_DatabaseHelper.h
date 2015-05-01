5d03054eb444477b23de7537e9927d799f88b70c
Liam Mellor <liam.r.mellor@gmail.com>
2015-05-01 19:23:53 -0400

    some small changes

--- HalalMapper/NSObject_DatabaseHelper.h
+++ HalalMapper/NSObject_DatabaseHelper.h
@@ -9,17 +9,19 @@
 #import <Foundation/Foundation.h>
 #import <sqlite3.h>
 
-@interface NSObject_DatabaseHelper : NSObject
-{
+@interface NSObject_DatabaseHelper : NSObject {
     NSString *databasePath;
 }
 
-+(NSObject_DatabaseHelper*)getSharedInstance;
++ (NSObject_DatabaseHelper*) getSharedInstance;
+
 - (BOOL) createDB;
+
 - (BOOL) saveData:(NSString*)cartid name:(NSString*)name
          latitude:(NSString*)latitude longitude:(NSString*)longitude
             likes:(NSString*)likes dislikes:(NSString*)dislikes
          freepita:(NSString*)freepita drinkincluded(NSString*)drinkincluded greensauce(NSString*)greensauce;
+
 - (NSArray*) findByRegisterNumber:(NSString*)cartid;
 
 @end
