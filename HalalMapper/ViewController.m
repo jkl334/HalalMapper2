ce4efaf0ff34a3d83958f0151dee96a4d5412ee0
Liam Mellor <liam.r.mellor@gmail.com>
2015-05-01 19:42:22 -0400

    added carts bitch

--- HalalMapper/ViewController.m
+++ HalalMapper/ViewController.m
@@ -30,9 +30,12 @@
     
     //pushing placeholder data
     [dataManager saveData:@"1" name:@"W4th and Greene" latitude:@"40.729065" longitude:@"-73.995667" likes:@"20" dislikes:@"3" freepita:@"YES" drinkincluded:@"YES" greensauce:@"NO"];
-    [dataManager saveData:@"1" name:@"W4th and Greene" latitude:@"40.729065" longitude:@"-73.995667" likes:@"20" dislikes:@"3" freepita:@"YES" drinkincluded:@"YES" greensauce:@"NO"];
-    [dataManager saveData:@"1" name:@"W4th and Greene" latitude:@"40.729065" longitude:@"-73.995667" likes:@"20" dislikes:@"3" freepita:@"YES" drinkincluded:@"YES" greensauce:@"NO"];
-    [dataManager saveData:@"1" name:@"W4th and Greene" latitude:@"40.729065" longitude:@"-73.995667" likes:@"20" dislikes:@"3" freepita:@"YES" drinkincluded:@"YES" greensauce:@"NO"];
+    [dataManager saveData:@"2" name:@"W4th and Washington Sqr East" latitude:@"40.729224" longitude:@"-73.996010" likes:@"16" dislikes:@"11" freepita:@"NO" drinkincluded:@"NO" greensauce:@"YES"];
+    [dataManager saveData:@"3" name:@"W Houston and Broadway" latitude:@"40.725374" longitude:@"-73.996959" likes:@"26" dislikes:@"14" freepita:@"NO" drinkincluded:@"YES" greensauce:@"YES"];
+    [dataManager saveData:@"4" name:@"Broadway and Astor" latitude:@"40.729864" longitude:@"-73.992898" likes:@"2" dislikes:@"16" freepita:@"NO" drinkincluded:@"NO" greensauce:@"NO"];
+    [dataManager saveData:@"5" name:@"Rafiqis E8th and Broadway" latitude:@"40.730693" longitude:@"-73.992190" likes:@"27" dislikes:@"13" freepita:@"YES" drinkincluded:@"YES" greensauce:@"YES"];
+    [dataManager saveData:@"6" name:@"E14th and 3rd Ave" latitude:@"40.733265" longitude:@"-73.987513" likes:@"18" dislikes:@"15" freepita:@"NO" drinkincluded:@"YES" greensauce:@"NO"];
+    [dataManager saveData:@"7" name:@"E14th and 4th Ave" latitude:@"40.734403" longitude:@"-73.989551" likes:@"12" dislikes:@"15" freepita:@"NO" drinkincluded:@"YES" greensauce:@"YES"];
     
     GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:40.7286689
                                                             longitude:-73.99566199999998
