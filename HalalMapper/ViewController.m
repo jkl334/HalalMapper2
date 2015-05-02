707401c3f9e2ef7121284f968aaafb0b5f77bf32
Jeff Lee <User@172-16-229-6.DYNAPOOL.NYU.EDU>
2015-05-01 21:52:20 -0400

    youngduck

--- HalalMapper/ViewController.m
+++ HalalMapper/ViewController.m
@@ -44,7 +44,7 @@ nibBundleOrNil
     //departmentTextField.text year:yearTextField.text];
     BOOL yesss;
     [[NSObject_DatabaseHelper getSharedInstance] createDB];
-    yesss = [[NSObject_DatabaseHelper getSharedInstance]saveData:@"1" name:@"W4th and Greene" latitude:@"40.729065" longitude:@"-73.995667" likes:@"20" dislikes:@"3" freepita:@"YES" drinkincluded:@"YES" greensauce:@"NO"];
+    [[NSObject_DatabaseHelper getSharedInstance]saveData:@"1" name:@"W4th and Greene" latitude:@"40.729065" longitude:@"-73.995667" likes:@"20" dislikes:@"3" freepita:@"YES" drinkincluded:@"YES" greensauce:@"NO"];
    /*
     //pushing placeholder data
     [dataManager saveData:@"1" name:@"W4th and Greene" latitude:@"40.729065" longitude:@"-73.995667" likes:@"20" dislikes:@"3" freepita:@"YES" drinkincluded:@"YES" greensauce:@"NO"];
@@ -78,13 +78,15 @@ nibBundleOrNil
     
   
     NSArray *dankness = [[NSObject_DatabaseHelper getSharedInstance] findByCartId:@"1"];
+    NSLog(@"AOSDOASD");
+    NSLog([[dankness objectAtIndex:0] stringValue]);
     
     
-    float latitude1 = [dankness[2] integerValue];
+    float latitude1  = [dankness[2] integerValue];
     float longitude2 = [dankness[3] integerValue];
     GMSMarker *cart1 = [[GMSMarker alloc] init];
-    cart1.position = CLLocationCoordinate2DMake(latitude1, longitude2);
-    cart1.title    = @"HEY";
+    cart1.position = CLLocationCoordinate2DMake(40.729065, -73.995667);
+    cart1.title    = [[dankness objectAtIndex:0] stringValue];
     cart1.snippet  = @"likes:200 dislikes: 32";
     cart1.map      = mapView_;
     
